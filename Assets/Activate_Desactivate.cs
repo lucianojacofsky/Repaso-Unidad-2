8bded1851a180ae10338e139830de0d0095d0a26
lucianojacofsky <101577485+lucianojacofsky@users.noreply.github.com>
2022-05-19 08:49:24 -0300

    Initial commit

---
+++ Assets/Activate_Desactivate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Activate_Desactivate : MonoBehaviour
+{
+    public GameObject Inputfield;
+    public bool wasPressed;
+    void start()
+    {
+        wasPressed = false;
+    }
+    
+    public void activateAnddesactivate()
+    {
+        if(!wasPressed)
+        {
+            Inputfield.SetActive(false);
+            wasPressed = true;
+        }
+        else
+        {
+            Inputfield.SetActive(true);
+            wasPressed = false;
+        }
+       
+            
+    }
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Repaso-Unidad-2
+ 
